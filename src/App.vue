bd831e61486acf502299fbb6e4b423cfd7c7039c
han-xi <50067998+han-xi@users.noreply.github.com>
2020-08-03 16:11:42 +0800

    整理页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <template>
-  <div id="app">
-
+  <div id="app" style="background-image: url(https://picsum.photos/1360/630)"> 
+<div class="foot" >
+    <p>Patent Miner ©2019-2020</p>
+</div>
     <router-view/>
   </div>
-</template>
 
+</template>
+<style scoped>
+    .foot{
+			bottom:0;
+      width:100%;
+      height:40px;
+			background-color:#b6b6b6;
+      text-align: center;
+      font-family: "宋体";
+      font-size: 12px;
+      color: #000;  
+      /* padding:25px 45px 30px; */
+			margin:0 auto;
+			border-top: 6px solid #eff1f1;   
+      color: #666666;
+            /* margin-top: 30px;
+            line-height: 2em; */
+      position: absolute;
+
+		}
+</style>
 
