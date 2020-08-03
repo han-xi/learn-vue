3f030ee59b5ad6bf3f3b8af755ba49080891103c
han-xi <50067998+han-xi@users.noreply.github.com>
2020-08-03 16:09:41 +0800

    敏感信息隐藏

--- server/mongoClient.py
+++ server/mongoClient.py
@@ -2,7 +2,7 @@ from bson.codec_options import DEFAULT_CODEC_OPTIONS
 from pymongo import MongoClient
 import pymongo
 from datetime import datetime
-
+from getsecertinfo import secert
 
 options = DEFAULT_CODEC_OPTIONS.with_options(
     unicode_decode_error_handler='ignore')
@@ -10,10 +10,11 @@ print('codec options ---', options.unicode_decode_error_handler)
 
 
 class MongoDBClient233(object):
-    def __init__(self, host='localhost', port=27017, db_name='info'):
+    def __init__(self,host='localhost',port=27017,db_name='info'):
+        server_info=secert()
         self.client = MongoClient(
-            host, port, unicode_decode_error_handler='ignore')
-        self.db = self.client.get_database(db_name, codec_options=options)
+            server_info.server['host'],  server_info.server['port'], unicode_decode_error_handler='ignore')
+        self.db = self.client.get_database( server_info.server['db_name'], codec_options=options)
         print("233 mongo connected")
         self.info = self.db["info"]
         self.mail = self.db["mail"]
