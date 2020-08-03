88b0bb68dc8854b5690876c901f80456dffecdd2
han-xi <50067998+han-xi@users.noreply.github.com>
2020-08-03 16:05:58 +0800

    加入islogin检验

--- server/app.py
+++ server/app.py
@@ -1,45 +1,39 @@
-from flask import Flask,request,session,redirect,jsonify,Response
+from flask import Flask,g,request,session,redirect,jsonify,Response
 from flask_cors import CORS
-
+from mongoClient import MongoDBClient233
 from flask import jsonify
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
 
-from userLogin import userLogin
-from userRegister import userRegister
-from userMail import userMail
-from userUpdate import userUpdate
-from userQuit import userQuit
+from apis.user import user
 import datetime
 
-global username
-# from search import search
-app.register_blueprint(userLogin, url_prefix='/')
-app.register_blueprint(userRegister, url_prefix='/')
-app.register_blueprint(userMail, url_prefix='/')
-app.register_blueprint(userUpdate, url_prefix='/')
-app.register_blueprint(userQuit, url_prefix='/')
-
+app.register_blueprint(user, url_prefix='/user')
 app.secret_key='\xca\x0c\x86\x04\x98@\x02b\x1b7\x8c\x88]\x1b\xd7"+\xe6px@\xc3#\\'
 app.permanent_session_lifetime=datetime.timedelta(days=14)
 
 @app.before_request#执行所有装饰器都要执行当前装饰器(简洁版实现同样功能)
 def login_required():
-    #request.cookies
-    if request.path in ['/userLogin','/userRegister','/userMail','/userUpdate']: #如果登录的路由是注册和登录就返会none
+    if request.path in ['/user/login','/user/register','/user/forget','/user/active','/user/changepassword']: #如果登录的路由是注册和登录就返会none
+        return None    
+    try:
+        username=session.get('user_id')  #获取用户登录信息
+    except:
+        return jsonify(''),400
+    if username :
+        g.username=username
         return None
-    print("------")
-    #name=request.cookies.get("u_uuid")
-    #print(name)
-    #print(session['user_id'])
-    #print(session.getId())
-    global username
-    username=session.get('user_id')  #获取用户登录信息
-    if  username  :                 #没有登录就自动跳转到登录页面去
-       return None 
-    return jsonify({"errCode":2}),401
+    # if  username :                 #没有登录就自动跳转到登录页面去
+    #     g.username=username
+    #     return None 
+    # elif userinfo['islogin']==True:#失效就更改登录标志
+    #     client.info.update({"username":g.username},{"$set":{"islogin":False}})
+    #     return jsonify({"errCode":2}),401
+    else:
+        return jsonify({"errCode":2}),401
+
 
 @app.after_request
 def after_request(response):
@@ -50,4 +44,4 @@ def after_request(response):
     return response
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=9004, threaded=True, debug=True)
+    app.run(host='127.0.0.1', port=9004, threaded=True, debug=True)
