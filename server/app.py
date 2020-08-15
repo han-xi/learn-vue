4de832352c239f7359c6572478bcb0d69c16132a
han-xi <50067998+han-xi@users.noreply.github.com>
2020-08-15 21:54:44 +0800

    修改登陆验证

--- server/app.py
+++ server/app.py
@@ -3,36 +3,17 @@ from flask_cors import CORS
 from mongoClient import MongoDBClient233
 from flask import jsonify
 
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
 
 from apis.user import user
-import datetime
+from datetime import datetime
+import config
 
+app.config.from_pyfile('config.py',silent=True)
 app.register_blueprint(user, url_prefix='/user')
-app.secret_key='\xca\x0c\x86\x04\x98@\x02b\x1b7\x8c\x88]\x1b\xd7"+\xe6px@\xc3#\\'
-app.permanent_session_lifetime=datetime.timedelta(days=14)
-
-@app.before_request#执行所有装饰器都要执行当前装饰器(简洁版实现同样功能)
-def login_required():
-    if request.path in ['/user/login','/user/register','/user/forget','/user/active','/user/changepassword']: #如果登录的路由是注册和登录就返会none
-        return None    
-    try:
-        username=session.get('user_id')  #获取用户登录信息
-    except:
-        return jsonify(''),400
-    if username :
-        g.username=username
-        return None
-    # if  username :                 #没有登录就自动跳转到登录页面去
-    #     g.username=username
-    #     return None 
-    # elif userinfo['islogin']==True:#失效就更改登录标志
-    #     client.info.update({"username":g.username},{"$set":{"islogin":False}})
-    #     return jsonify({"errCode":2}),401
-    else:
-        return jsonify({"errCode":2}),401
 
 
 @app.after_request
@@ -40,8 +21,20 @@ def after_request(response):
     #response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,session_id')
     #response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,HEAD')
     # 这里不能使用add方法，否则会出现 The 'Access-Control-Allow-Origin' header contains multiple values 的问题
+    try:
+        if g.token:#每次登录时设置token
+            response.set_cookie('token',g.token,httponly=True,max_age=3600)
+        if g.levelendtime:
+            response.set_cookie('levelendtime',g.levelendtime,httponly=True,max_age=3600)
+
+    except:
+        pass
     response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response
-
+#ServerSelectionTimeoutError
+@app.errorhandler(404)# 参数是错误代码
+def error404(error_info):# 注意，一定要加参数接收错误信息 
+    # 404 Not Found: The requested URL was not found on the server.  If you entered the URL manually please check your spelling and try again.
+    return jsonify({"error":'请求错误'})# 可以返回 三剑客 + 小儿子　@app.errorhandler(404)# 参数是错误代码
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=9004, threaded=True, debug=True)
