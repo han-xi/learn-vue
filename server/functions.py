04040019c5cacef7fe928610f0e4385876c24122
han-xi <50067998+han-xi@users.noreply.github.com>
2020-08-15 21:55:33 +0800

    修改token生成和检验

--- server/functions.py
+++ server/functions.py
@@ -5,14 +5,13 @@ import json
 # smtplib 用于邮件的发信动作
 import smtplib
 from email.mime.text import MIMEText
-from flask import jsonify, Blueprint,request
+from flask import jsonify, Blueprint,request,current_app
 # email 用于构建邮件内容
 from email.header import Header
 from datetime import datetime
 from getsecertinfo import secert
-from mongoClient import MongoDBClient233
 import re
-
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 def validateEmail(s):
     temp="^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$"
@@ -38,10 +37,36 @@ def md5value(s):
 def genRandomString(slen=10):
     return ''.join(random.sample(string.ascii_letters + string.digits, slen))
 
+def create_token(user_name, role_list):
+    s = Serializer(current_app.config["SECRET_KEY"], expires_in=current_app.config["TOKEN_EXPIRATION"])
+    # 接收用户id转换与编码
+    token = None
+    try:
+        token = s.dumps({"username": user_name, "role": role_list}).decode("ascii")
+    except Exception as e:
+        current_app.logger.error("获取token失败:{}".format(e))
+    return token
+    
+def verify_token(token,*role):#是tuple类型role否则list(role)会出错
+    s = Serializer(current_app.config["SECRET_KEY"])
+    try:
+        # 转换为字典
+        data = s.loads(token)
+        token_role=data.get('role').split(' ')
+        if token_role:
+            # 获取token中的权限列表如果在参数列表中则表示有权限，否则就表示没有权限
+            #user_role = current_app.config['ROLE_LIST']
+            result = [x for x in list(role) if x in token_role]
+            if not result:
+                return 1
+            else:
+                return data
+    except Exception as e:
+        return 2
 def send_email(mail_name,client,send_type):
     server_info=secert()
     if send_type == 'forget':
-        random_code = genRandomString(4)
+        random_code = genRandomString(8)
     else:
         random_code = genRandomString(6)
     # 发信方的信息：发信邮箱，QQ 邮箱授权码
@@ -49,7 +74,6 @@ def send_email(mail_name,client,send_type):
     password = server_info.mail['mail_password']
     # 收信方邮箱
     to_addr = mail_name
-    print(type(to_addr))
     # 发信服务器
     smtp_server = server_info.mail['smtp_server']
     #端口
@@ -83,8 +107,9 @@ def send_email(mail_name,client,send_type):
     except:
         return False
     else:
+
         try:
-            client.mail.insert_one({'type':send_type,'username':mail_name,'code':random_code,'isactive':False})
+            client.mail.insert_one({'type':send_type,'username':mail_name,'code':random_code,'isactive':False,'send_time':datetime.now()})
         except:
             return False
         return True
